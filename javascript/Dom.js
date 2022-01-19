052d6a8ad7c1a3b75f4536ab13b4ce2019986bea
ArthurFernande <arthur.andrade@ccc.ufcg.edu.br>
2022-01-19 00:41:02 -0300

    estudando dom

---
+++ javascript/Dom.js
@@ -0,0 +1,6 @@
+/*document.getElementById("exemplo").innerHTML = "Pedro<button>botão</button>";*/
+
+document.getElementById("exemplo");
+document.getElementsByClassName("lista")[0].innerHTML = "Item alterado";
+document.getElementsByTagName('button');
+document.getElementsByName('email');
